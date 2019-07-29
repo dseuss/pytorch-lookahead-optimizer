c7615be89da8623b51b25b1baa5ec9ded7dc11c6
Daniel Suess <daniel@dsuess.me>
2019-07-29 17:31:15 +1000

    Refactor LookaheadOptimizer to work with lr scheduler

--- optim.py
+++ optim.py
@@ -1,9 +1,11 @@
+from collections import defaultdict
 import itertools as it
 import math
 from copy import deepcopy
 
 import torch
 from torch.optim.optimizer import Optimizer
+
 
 
 class LookaheadOptimizer(Optimizer):
@@ -16,21 +18,18 @@ class LookaheadOptimizer(Optimizer):
         if not issubclass(fast_optimizer, Optimizer):
             raise ValueError(f'Invalid fast_optimizer: {fast_optimizer}')
 
-        params = list(params)
         self.fast_optimizer = fast_optimizer(params, **kwargs)
 
         # TODO Proper handling of params for fast optimizer
-        defaults = dict(
-            **self.fast_optimizer.defaults,
+        self.defaults = dict(
             slow_update_rate=slow_update_rate,
             lookahead_steps=lookahead_steps,
             step_counter=0)
+        self.state = defaultdict(dict)
 
-        super().__init__(params=params, defaults=defaults)
-        self.slow_weights = [[w.clone().detach() for w in g['params']]
-                             for g in self.param_groups]
-        for w in it.chain(*self.slow_weights):
-            w.requires_grad = False
+        for group in self.fast_optimizer.param_groups:
+            for name, value in self.defaults.items():
+                group[name] = group.get(name, value)
 
     def step(self, closure=None):
         loss = None
@@ -39,15 +38,31 @@ class LookaheadOptimizer(Optimizer):
 
         self.fast_optimizer.step()
 
-        for group, slow_weights in zip(self.param_groups, self.slow_weights):
+        for group in self.param_groups:
             group['step_counter'] += 1
             if group['step_counter'] < group['lookahead_steps']:
                 continue
 
             alpha = group['slow_update_rate']
-            for w_fast, w_slow in zip(group['params'], slow_weights):
-                w_fast.data = alpha * w_fast.data + (1 - alpha) * w_slow.data
-                w_fast.grad = None
-                w_slow.data = w_fast.data
+            for tensor in group['params']:
+                state = self.state[tensor]
+                if len(state) == 0:
+                    state['slow_weight'] = tensor.clone().detach()
+                    state['slow_weight'].requires_grad = False
+
+                slow_weight = state['slow_weight']
+                tensor.data = alpha * tensor.data + (1 - alpha) * slow_weight
+                tensor.grad = None
+                slow_weight.data = tensor.data
 
             group['step_counter'] = 0
+
+    def add_param_group(self, group):
+        self.fast_optimizer.add_param_group(group)
+        group = self.fast_optimizer.param_groups[-1]
+        for name, value in self.defaults.items():
+            group[name] = group.get(name, value)
+
+    @property
+    def param_groups(self):
+        return self.fast_optimizer.param_groups
